90d465a806477d84dd49bbba2059600652173904
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-01 17:25:07 +0100

    Add test for loading local data

---
+++ tests/test_loaders.py
@@ -0,0 +1,24 @@
+import os
+import pytest
+import em27_metadata
+import dotenv
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+@pytest.mark.library
+def test_remote_loader() -> None:
+    dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+    em27_metadata.load_from_github(
+        "tum-esm/em27-metadata",
+        access_token=os.getenv("GITHUB_ACCESS_TOKEN"),
+    )
+
+
+@pytest.mark.library
+def test_local_loader() -> None:
+    em27_metadata.load_from_local_files(
+        locations_path=os.path.join(PROJECT_DIR, "data", "locations.json"),
+        sensors_path=os.path.join(PROJECT_DIR, "data", "sensors.json"),
+        campaigns_path=os.path.join(PROJECT_DIR, "data", "campaigns.json"),
+    )
