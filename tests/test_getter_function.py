94af43821c153b1e8e8ac01407b384d01abd3897
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-01 18:23:44 +0100

    Simplify the schema (11)
    Refactor getter test

--- tests/test_getter_function.py
+++ tests/test_getter_function.py
@@ -5,134 +5,108 @@ import em27_metadata
 
 @pytest.mark.library
 def test_getter_function() -> None:
-    locations = [
-        em27_metadata.types.LocationMetadata(
-            **{
-                "location_id": "lid1",
-                "details": "description of location 1",
-                "lon": 10.5,
-                "lat": 48.1,
-                "alt": 500,
-            }
-        ),
-        em27_metadata.types.LocationMetadata(
-            **{
-                "location_id": "lid2",
-                "details": "description of location 2",
-                "lon": 11.3,
-                "lat": 48.0,
-                "alt": 600,
-            }
-        ),
-    ]
-    sensors = [
-        em27_metadata.types.SensorMetadata.model_validate({
-            "sensor_id":
-                "sid1",
-            "serial_number":
-                51,
-            "different_utc_offsets": [
-                {
-                    "from_datetime": "2020-02-01T02:00:00+00:00",
-                    "to_datetime": "2020-02-01T15:59:59+00:00",
-                    "utc_offset": 1,
-                },
-                {
-                    "from_datetime": "2020-02-01T16:00:00+00:00",
-                    "to_datetime": "2020-02-01T21:59:59+00:00",
-                    "utc_offset": 2,
-                },
-            ],
-            "different_pressure_data_sources": [
-                {
-                    "from_datetime": "2020-02-01T02:00:00+00:00",
-                    "to_datetime": "2020-02-01T14:59:59+00:00",
-                    "source": "src1",
-                },
-                {
-                    "from_datetime": "2020-02-01T15:00:00+00:00",
-                    "to_datetime": "2020-02-01T21:59:59+00:00",
-                    "source": "src2",
-                },
-            ],
-            "different_calibration_factors": [
-                {
-                    "from_datetime": "2020-02-01T02:00:00+00:00",
-                    "to_datetime": "2020-02-01T12:59:59+00:00",
-                    "pressure": 1.001,
-                    "xco2": {
-                        "factors": [1.001, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                    "xch4": {
-                        "factors": [1.002, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                    "xco": {
-                        "factors": [1.003, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                },
-                {
-                    "from_datetime": "2020-02-01T13:00:00+00:00",
-                    "to_datetime": "2020-02-01T13:59:59+00:00",
-                    "pressure": 1.001,
-                    "xco2": {
-                        "factors": [1.004, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                    "xch4": {
-                        "factors": [1.005, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                    "xco": {
-                        "factors": [1.006, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                },
-                {
-                    "from_datetime": "2020-02-01T14:00:00+00:00",
-                    "to_datetime": "2020-02-01T21:59:59+00:00",
-                    "pressure": 1.002,
-                    "xco2": {
-                        "factors": [1.004, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                    "xch4": {
-                        "factors": [1.005, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                    "xco": {
-                        "factors": [1.006, 0],
-                        "scheme": "Ohyama2021",
-                    },
-                },
-            ],
-            "locations": [
-                {
-                    "from_datetime": "2020-02-01T01:00:00+00:00",
-                    "to_datetime": "2020-02-01T09:59:59+00:00",
-                    "location_id": "lid1",
-                },
-                {
-                    "from_datetime": "2020-02-01T12:00:00+00:00",
-                    "to_datetime": "2020-02-01T22:59:59+00:00",
-                    "location_id": "lid2",
-                },
-            ],
-        }),
-    ]
-
-    location_data = em27_metadata.interfaces.EM27MetadataInterface(
-        locations, sensors, campaigns=[]
+    locations = em27_metadata.types.LocationMetadataList(
+        root=[
+            em27_metadata.types.LocationMetadata(
+                location_id="lid1",
+                details="description of location 1",
+                lon=10.5,
+                lat=48.1,
+                alt=500,
+            ),
+            em27_metadata.types.LocationMetadata(
+                location_id="lid2",
+                details="description of location 2",
+                lon=11.3,
+                lat=48.0,
+                alt=600,
+            ),
+        ]
+    )
+    sensors = em27_metadata.types.SensorMetadataList(
+        root=[
+            em27_metadata.types.SensorMetadata(
+                sensor_id="sid1",
+                serial_number=51,
+                calibration_factors=[
+                    em27_metadata.types.CalibrationFactorsListItem(
+                        from_datetime="2020-02-01T02:00:00+0000",
+                        to_datetime="2020-02-01T12:59:59+0000",
+                        value=em27_metadata.types.CalibrationFactors(
+                            pressure=1.001,
+                            xco2=em27_metadata.types.
+                            GasSpecificCalibrationFactors(
+                                factors=[1.002, 0],
+                                scheme="Ohyama2021",
+                            ),
+                            xch4=em27_metadata.types.
+                            GasSpecificCalibrationFactors(
+                                factors=[1.003, 0],
+                                scheme="Ohyama2021",
+                            ),
+                            xco=em27_metadata.types.
+                            GasSpecificCalibrationFactors(
+                                factors=[1.004, 0],
+                                scheme="Ohyama2021",
+                            ),
+                        ),
+                    ),
+                    em27_metadata.types.CalibrationFactorsListItem(
+                        from_datetime="2020-02-01T13:00:00+0000",
+                        to_datetime="2020-02-01T13:59:59+0000",
+                        value=em27_metadata.types.CalibrationFactors(
+                            pressure=1.005,
+                            xco2=em27_metadata.types.
+                            GasSpecificCalibrationFactors(
+                                factors=[1.006, 0],
+                                scheme="Ohyama2021",
+                            ),
+                            xch4=em27_metadata.types.
+                            GasSpecificCalibrationFactors(
+                                factors=[1.007, 0],
+                                scheme="Ohyama2021",
+                            ),
+                            xco=em27_metadata.types.
+                            GasSpecificCalibrationFactors(
+                                factors=[1.008, 0],
+                                scheme="Ohyama2021",
+                            ),
+                        ),
+                    )
+                ],
+                setups=[
+                    em27_metadata.types.SetupsListItem(
+                        from_datetime="2020-02-01T01:00:00+0000",
+                        to_datetime="2020-02-01T09:59:59+0000",
+                        value=em27_metadata.types.Setup(
+                            location_id="lid1",
+                            pressure_data_source="another",
+                            utc_offset=3,
+                        ),
+                    ),
+                    em27_metadata.types.SetupsListItem(
+                        from_datetime="2020-02-01T12:00:00+0000",
+                        to_datetime="2020-02-01T22:59:59+0000",
+                        value=em27_metadata.types.Setup(location_id="lid2", ),
+                    ),
+                ]
+            ),
+        ]
     )
 
-    from_datetime = datetime.datetime.fromisoformat("2020-02-01T00:00:00+00:00")
-    to_datetime = datetime.datetime.fromisoformat("2020-02-01T23:59:59+00:00")
+    metadata = em27_metadata.interfaces.EM27MetadataInterface(
+        locations,
+        sensors,
+        campaigns=em27_metadata.types.CampaignMetadataList(root=[]),
+    )
 
-    chunks = location_data.get("sid1", from_datetime, to_datetime)
+    from_datetime = datetime.datetime.fromisoformat("2020-02-01T00:00:00+0000")
+    to_datetime = datetime.datetime.fromisoformat("2020-02-01T23:59:59+0000")
 
-    assert len(chunks) == 8
+    chunks = metadata.get("sid1", from_datetime, to_datetime)
+    # 1-2, 2-10, 12-13, 13-14, 14-23
+
+    assert len(chunks) == 5
 
     # check correct splitting
 
@@ -143,9 +117,6 @@ def test_getter_function() -> None:
         datetime.datetime.fromisoformat("2020-02-01T12:00:00+00:00"),
         datetime.datetime.fromisoformat("2020-02-01T13:00:00+00:00"),
         datetime.datetime.fromisoformat("2020-02-01T14:00:00+00:00"),
-        datetime.datetime.fromisoformat("2020-02-01T15:00:00+00:00"),
-        datetime.datetime.fromisoformat("2020-02-01T16:00:00+00:00"),
-        datetime.datetime.fromisoformat("2020-02-01T22:00:00+00:00"),
     ]
     to_datetimes = [c.to_datetime for c in chunks]
     assert to_datetimes == [
@@ -153,36 +124,21 @@ def test_getter_function() -> None:
         datetime.datetime.fromisoformat("2020-02-01T09:59:59+00:00"),
         datetime.datetime.fromisoformat("2020-02-01T12:59:59+00:00"),
         datetime.datetime.fromisoformat("2020-02-01T13:59:59+00:00"),
-        datetime.datetime.fromisoformat("2020-02-01T14:59:59+00:00"),
-        datetime.datetime.fromisoformat("2020-02-01T15:59:59+00:00"),
-        datetime.datetime.fromisoformat("2020-02-01T21:59:59+00:00"),
         datetime.datetime.fromisoformat("2020-02-01T22:59:59+00:00"),
     ]
 
+    location_ids = [c.location.location_id for c in chunks]
+    assert location_ids == ["lid1", "lid1", "lid2", "lid2", "lid2"]
+
     utc_offsets = [c.utc_offset for c in chunks]
-    assert utc_offsets == [0, 1, 1, 1, 1, 1, 2, 0]
+    assert utc_offsets == [1, 1, 0, 0, 0]
 
     pressure_data_sources = [c.pressure_data_source for c in chunks]
     assert pressure_data_sources == [
-        "sid1", "src1", "src1", "src1", "src1", "src2", "src2", "sid1"
+        "another", "another", "sid1", "sid1", "sid1"
     ]
 
     pressure_calibration_factors = [
         c.calibration_factors.pressure for c in chunks
     ]
-    assert pressure_calibration_factors == [
-        1.0, 1.001, 1.001, 1.001, 1.002, 1.002, 1.002, 1.0
-    ]
-
-    xco2_calibration_factors = [(
-        None if (c.calibration_factors.xco2 is None) else
-        c.calibration_factors.xco2.factors[0]
-    ) for c in chunks]
-    assert xco2_calibration_factors == [
-        1, 1.001, 1.001, 1.004, 1.004, 1.004, 1.004, 1
-    ]
-
-    location_ids = [c.location.location_id for c in chunks]
-    assert location_ids == [
-        "lid1", "lid1", "lid2", "lid2", "lid2", "lid2", "lid2", "lid2"
-    ]
+    assert pressure_calibration_factors == [1.0, 1.001, 1.001, 1.005, 1.0]
