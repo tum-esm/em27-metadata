79e2e1d1404165967f9af05a6c7410081e4f6a66
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-24 22:37:29 +0100

    Migrate from Poetry to PDM (3)
    Update test.yml

--- tests/test_static_types.py
+++ tests/test_static_types.py
@@ -1,17 +1,22 @@
 import os
+import sys
 import pytest
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 @pytest.mark.library
 def test_static_types() -> None:
     print("removing old mypy cache")
-    r1 = os.system("rm -rf .mypy_cache")
+    r1 = os.system(f"cd {PROJECT_DIR} && rm -rf .mypy_cache")
     assert r1 == 0, "command returned non-zero exit code"
 
     print("checking em27_metadata/")
-    r2 = os.system(".venv/bin/python -m mypy em27_metadata/")
+    r2 = os.system(
+        f"cd {PROJECT_DIR} && {sys.executable} -m mypy em27_metadata/"
+    )
     assert r2 == 0, "command returned non-zero exit code"
 
     print("checking tests/")
-    r3 = os.system(".venv/bin/python -m mypy tests/")
+    r3 = os.system(f"cd {PROJECT_DIR} && {sys.executable} -m mypy tests/")
     assert r3 == 0, "command returned non-zero exit code"
