71b27e571a7313bafe2c08ea8a77ff4c19e735f3
Moritz Makowski <moritz@dostuffthatmatters.dev>
2025-11-05 12:05:18 +0100

    Improve static type validation (3)
    Add pyright to CI tests

--- tests/test_static_types.py
+++ tests/test_static_types.py
@@ -6,9 +6,11 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 @pytest.mark.library
-def test_static_types() -> None:
+def test_with_mypy() -> None:
     print("removing old mypy cache")
-    r1 = os.system(f"cd {PROJECT_DIR} && rm -rf .mypy_cache")
+    r1 = os.system(
+        f"cd {PROJECT_DIR} && rm -rf .mypy_cache/3.*/em27_metadata && rm -rf .mypy_cache/3.*/tests"
+    )
     assert r1 == 0, "command returned non-zero exit code"
 
     print("checking em27_metadata/")
@@ -18,3 +20,8 @@ def test_static_types() -> None:
     print("checking tests/")
     r3 = os.system(f"cd {PROJECT_DIR} && {sys.executable} -m mypy tests/")
     assert r3 == 0, "command returned non-zero exit code"
+
+
+@pytest.mark.library
+def test_with_pyright() -> None:
+    assert os.system(f"cd {PROJECT_DIR} && {sys.executable} -m pyright") == 0
